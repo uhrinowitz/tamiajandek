069b84c786ce670929fba265ef23445890cf06c8
uhrinowitz <62422000+uhrinowitz@users.noreply.github.com>
2020-12-03 17:44:29 +0100

    dee

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // SETTING UP THE SERVER
 var express=require("express");
 var app=express();
+
 var server=require("http").Server(app);
+var io = require('socket.io')(server);
 let appPort = process.env.PORT || 4200;
 
 // FILE SERVER SECTION
 app.get('/', (req, res) => {  
-    res.sendFile(__dirname + 'tami.html');
+    res.sendFile(__dirname + '/tami.html');
 
 
     
